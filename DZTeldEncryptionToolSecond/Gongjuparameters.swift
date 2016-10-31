6eb3bc8164b4d29a0841fa212bf6b97bd2e9c661
hanxueshi <hanxs@HanXS.local>
2016-10-31 11:30:42 +0800

    hah
    hah

---
+++ DZTeldEncryptionToolSecond/Gongjuparameters.swift
@@ -0,0 +1,68 @@
+//
+//  gongjuparameters.swift
+//  qlwbapp
+//
+//  Created by apple on 15/7/2.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+//MARK:----------------------------APP 配置信息--------------------------------------下
+//////准生产环境api  asdad
+let GLOBAL_IPADDRESS_API = "http:///"
+let GLOBAL_IPADDRESS_PIC = "http:///"
+
+let GLOBAL_THIS_APPNAME = "来车"
+
+let GLOBAL_CREDIT_FUNCTION = 1
+
+let GLOBAL_APPSTORE_VERSION = true
+
+let GLOBAL_APPSTORE_URL = "https:///lookup?id=1065420883"
+
+
+let GLOBAL_THIS_APPID = "082D63FA--4228-B758-B4D573AC2870"
+
+
+let GAODE_MAP_APPKEY = ""//2 1
+let GAODE_MAP_WEB_API = ""
+
+let UMENG_APP_KEY = ""
+let UMENG_SHARE_SINA_APPKEY = ""
+let UMENG_SHARE_SINA_SECRET = ""
+let UMENG_SHARE_SINA_REDIRECTURL = ""
+
+
+let BUGLY_APP_ID  = "0c064ac5ce"
+
+let GLOBAL_MASTER_COLOR = UIColor(red: 121/255, green: 188/255, blue: 38/255, alpha: 1)
+
+
+///
+let GETUI_APPID = ""
+
+let GETUI_APPKEY = ""
+
+let GETUI_APP_SECRET = ""
+
+let GETUI_MASTER_SECRET = ""
+
+
+
+let GLOBAL_PINGPP_APPKEY = ""
+
+let GLOBAL_WX_APPKEY = ""
+
+let GLOBAL_WX_SECRET = ""
+
+/// 热门城市相关信息;
+struct HotCityInfo{
+    var cityName:String = "青岛市"
+    var cityCode:String = "3702"
+    var cityLatitude:Double = 36.067082
+    var cityLongitude:Double = 120.382640
+}
+//MARK:----------------------------APP 配置信息--------------------------------------上
+
